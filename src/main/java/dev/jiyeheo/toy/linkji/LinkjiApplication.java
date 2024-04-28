8d4c2fc325c3f7fec533babd812967532441befa
HeoJiye <heoze1031@gmail.com>
2024-04-28 18:56:07 +0900

    project init
    - gradle spring 프로젝트 생성
    - vite React  프로젝트 생성

---
+++ src/main/java/dev/jiyeheo/toy/linkji/LinkjiApplication.java
@@ -0,0 +1,13 @@
+package dev.jiyeheo.toy.linkji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkjiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jiyeheo/toy/linkji/Linkji2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jiyeheo.toy.linkji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Linkji2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
